8b3dc6a6985ac02b26445d2a5bde7c6cf3d1fbca
Gustav Behm <gustav.behm@magine.com>
2016-10-31 10:46:03 +0100

    Simplify the ProofAssistantExample by removing the uninteressting Proof functor

--- src/test/scala/ProofAssistantExample.scala
+++ src/test/scala/ProofAssistantExample.scala
@@ -5,40 +5,39 @@ class ProofAssistantExample extends WordSpec with Matchers {
   "ProofAssistantExample" should {
     class A
     class B
-    class Proof[P]
 
     "not prove something without proof" in {
-      illTyped { """implicitly[Proof[A]]""" }
+      illTyped { """implicitly[A]""" }
     }
 
     "prove A with proof of A" in {
-      implicit val proof = new Proof[A]
-      implicitly[Proof[A]] shouldBe proof
-      illTyped { """implicitly[Proof[B]]""" }
+      implicit val proof = new A
+      implicitly[A] shouldBe proof
+      illTyped { """implicitly[B]""" }
     }
 
     "tuples are conjunction" when {
       implicit def conjuction[T, S](implicit t: T, s: S): (T,S) = (t,s)
 
       "A, B => (A and B)" in {
-        implicit val proofA = new Proof[A]
-        implicit val proofB = new Proof[B]
+        implicit val proofA = new A
+        implicit val proofB = new B
 
-        implicitly[(Proof[A], Proof[B])]
+        implicitly[(A, B)]
       }
 
       "not A, not B => not (A and B)" in {
-        illTyped { """implicitly[(Proof[A], Proof[B])]""" }
+        illTyped { """implicitly[(A, B)]""" }
       }
 
       "A, not B => not (A and B)" in {
-        implicit val proof = new Proof[A]
-        illTyped { """implicitly[(Proof[A], Proof[B])]""" }
+        implicit val proof = new A
+        illTyped { """implicitly[(A, B)]""" }
       }
 
       "not A, B => not (A and B)" in {
-        implicit val proof = new Proof[B]
-        illTyped { """implicitly[(Proof[A], Proof[B])]""" }
+        implicit val proof = new B
+        illTyped { """implicitly[(A, B)]""" }
       }
     }
 
@@ -48,24 +47,24 @@ class ProofAssistantExample extends WordSpec with Matchers {
 
       // TODO: Can this ever be unambiguous?
       //"A, B => (A or B)" in {
-      //  implicit val proofA = new Proof[A]
-      //  implicit val proofB = new Proof[B]
+      //  implicit val proofA = new A
+      //  implicit val proofB = new B
 
-      //  implicitly[Either[Proof[A], Proof[B]]]
+      //  implicitly[Either[A, B]]
       //}
 
       "not A, not B => not (A or B)" in {
-        illTyped { """implicitly[Either[Proof[A], Proof[B]]]""" }
+        illTyped { """implicitly[Either[A, B]]""" }
       }
 
       "A, not B => (A or B)" in {
-        implicit val proof = new Proof[A]
-        implicitly[Either[Proof[A], Proof[B]]]
+        implicit val proof = new A
+        implicitly[Either[A, B]]
       }
 
       "not A, B => not (A and B)" in {
-        implicit val proof = new Proof[B]
-        implicitly[Either[Proof[A], Proof[B]]]
+        implicit val proof = new B
+        implicitly[Either[A, B]]
       }
     }
   }
