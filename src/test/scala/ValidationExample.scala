df31edabb2b8868028ecf13d94244a82fe08090a
Gustav Behm <gustav.behm@magine.com>
2016-11-24 21:40:31 +0100

    Add a ValidationExample

---
+++ src/test/scala/ValidationExample.scala
@@ -0,0 +1,21 @@
+import org.scalatest.{WordSpec, Matchers}
+
+import scalaz._, Scalaz._, NonEmptyList._
+
+class ValidationExample extends WordSpec with Matchers {
+  "ValidationExample" should {
+    "collect errors" in {
+      case class Raw(i: Int, s: String)
+      case class Parsed private (i: Int, s: String)
+      def parse(raw: Raw): ValidationNel[String, Parsed] = {
+        Applicative[ValidationNel[String, ?]].apply2(
+          raw.i.success.ensure("negative")(_ >= 0).toValidationNel,
+          raw.s.success.ensure("empty")(_.nonEmpty).toValidationNel) (Parsed)
+      }
+
+      parse(Raw(-1, "")) shouldBe nels("negative", "empty").failure
+      parse(Raw(1, "")) shouldBe nels("empty").failure
+      parse(Raw(1, "a")) shouldBe Parsed(1, "a").success
+    }
+  }
+}
